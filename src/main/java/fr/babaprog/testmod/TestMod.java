0e558dfcbe34b7383d0b08297c82d46692cb7404
BaptisteHardelin <baptiste.hardelin@hotmail.com>
2022-05-29 20:42:14 +0200

    Add custom item

--- src/main/java/fr/babaprog/testmod/TestMod.java
+++ src/main/java/fr/babaprog/testmod/TestMod.java
@@ -1,9 +1,11 @@
 package fr.babaprog.testmod;
 
+import fr.babaprog.testmod.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -25,7 +27,11 @@ public class TestMod
 
 
     public TestMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
     }
 

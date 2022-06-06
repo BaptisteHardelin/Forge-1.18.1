f2aed178c3a000206a31cdc72de66d15b5a0481a
BaptisteHardelin <baptiste.hardeli@hotmail.com>
2022-06-06 22:19:01 +0200

    Add custom tools and some fix

--- src/main/java/fr/babaprog/testmod/item/ModItems.java
+++ src/main/java/fr/babaprog/testmod/item/ModItems.java
@@ -3,7 +3,12 @@ package fr.babaprog.testmod.item;
 import fr.babaprog.testmod.TestMod;
 import fr.babaprog.testmod.item.custom.CoalCokeItem;
 import fr.babaprog.testmod.item.custom.DowsingRodItem;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,27 +16,49 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
 
-    // Liste d'item pour les items créés
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+        // Liste d'item pour les items créés
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+                        TestMod.MOD_ID);
 
-    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
+        public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+                        () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
 
-    public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
+        public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine",
+                        () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
 
-    public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod",
-            () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB).durability(16)));
+        public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod",
+                        () -> new DowsingRodItem(
+                                        new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB).durability(16)));
 
-    public static final RegistryObject<Item> CUCUMBER = ITEMS.register("cucumber",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB).food(ModFoods.CUCUMBER)));
+        public static final RegistryObject<Item> CUCUMBER = ITEMS.register("cucumber",
+                        () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)
+                                        .food(ModFoods.CUCUMBER)));
 
-    public static final RegistryObject<Item> COAL_COKE_ITEM = ITEMS.register("coal_coke",
-            () -> new CoalCokeItem(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
+        public static final RegistryObject<Item> COAL_COKE_ITEM = ITEMS.register("coal_coke",
+                        () -> new CoalCokeItem(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
 
+        public static final RegistryObject<Item> CITRINE_SWORD = ITEMS.register("citrine_sword",
+                        () -> new SwordItem(ModTiers.CITRINE,
+                                        2, 3f, new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
 
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+        public static final RegistryObject<Item> CITRINE_PICKAXE = ITEMS.register("citrine_pickaxe",
+                        () -> new PickaxeItem(ModTiers.CITRINE,
+                                        1, 1f, new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
+
+        public static final RegistryObject<Item> CITRINE_SHOVEL = ITEMS.register("citrine_shovel",
+                        () -> new ShovelItem(ModTiers.CITRINE,
+                                        1, 1f, new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
+
+        public static final RegistryObject<Item> CITRINE_AXE = ITEMS.register("citrine_axe",
+                        () -> new AxeItem(ModTiers.CITRINE,
+                                        1, 1f, new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
+
+        public static final RegistryObject<Item> CITRINE_HOUE = ITEMS.register("citrine_houe",
+                        () -> new HoeItem(ModTiers.CITRINE,
+                                        1, 1f, new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
+
+        public static void register(IEventBus eventBus) {
+                ITEMS.register(eventBus);
+        }
 
 }
